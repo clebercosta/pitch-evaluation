bd9737ce41a6f3b75a302cd1376067dd8c9d092d
clebercosta <me@clebercosta.com>
2026-03-17 20:19:30 -0300

    fix: separar api routes do bundle vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [],
+    }
+  },
+  optimizeDeps: {
+    exclude: ['@upstash/redis']
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
